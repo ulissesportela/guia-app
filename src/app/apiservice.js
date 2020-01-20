6d311e02585621bb1937eb77a9ca6aa93e61df69
Ulisses <ulisses.portela@gmail.com>
2020-01-19 22:19:47 -0300

    adicionando a url da api remota, e configuração da homepage

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -2,7 +2,8 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080'
+    //baseURL: 'http://localhost:8080'
+    baseURL: 'https://guia-api.herokuapp.com'
 })
 
 export default class ApiService {
